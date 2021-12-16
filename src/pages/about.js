d88373a57a18fcf954c753947c9c7da357f28cb0
adriana <adriana.mihalescu@gmail.com>
2021-12-16 16:15:52 +0000

    testing inline css, global css and css modules

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,10 +1,20 @@
 import React from "react"
 import Layout from "../components/Layout"
+import { page, text } from "../examples/about.module.css"
 
 const about = () => {
   return (
     <Layout>
-      <h2>hello from about page</h2>
+      <div className={page}>
+        <h1> about page</h1>
+        <h1>hello world</h1>
+        <p className={text}>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Omnis illum
+          quis aliquid distinctio commodi iure quae incidunt similique,
+          molestiae modi tempore dolor voluptates. Mollitia tempore aspernatur
+          ut quaerat, recusandae aliquam?
+        </p>
+      </div>
     </Layout>
   )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,22 @@
 import React from "react"
 import { Link } from "gatsby"
 import Layout from "../components/Layout"
+import { page, text } from "../examples/home.module.css"
 
 export default function Home() {
   return (
     <Layout>
-      <h1>Home page!</h1>
+      <div className={page}>
+        <h1>Home page!</h1>
+        <h1>hello world</h1>
+        <h1>hello people</h1>
+        <p className={text}>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quis modi
+          totam enim corporis harum sed, magni aspernatur distinctio optio
+          molestias iure repellendus laudantium vel quo dolorem quidem impedit
+          odit itaque!
+        </p>
+      </div>
       <div>
         <Link to="/about">about</Link>
       </div>
