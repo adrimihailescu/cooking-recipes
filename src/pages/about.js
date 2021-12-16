8f29fcfc24e866417edd5b366bbc765aeb0c5502
adriana <adriana.mihalescu@gmail.com>
2021-12-16 17:01:28 +0000

    removed all the filex and folders not necessary for the course

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,34 +1,12 @@
 import React from "react"
 import Layout from "../components/Layout"
-import { page, text } from "../examples/about.module.css"
-import styled from "styled-components"
 
 const about = () => {
   return (
     <Layout>
-      <Wrapper>
-        <h1> about page</h1>
-        <h1>hello world</h1>
-        <p className={text}>
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Omnis illum
-          quis aliquid distinctio commodi iure quae incidunt similique,
-          molestiae modi tempore dolor voluptates. Mollitia tempore aspernatur
-          ut quaerat, recusandae aliquam?
-        </p>
-      </Wrapper>
+      <h1> about page</h1>
     </Layout>
   )
 }
 
-const Wrapper = styled.section`
-  color: red;
-
-  h1 {
-    color: yellow;
-  }
-
-  .text {
-    text-transform: uppercase;
-  }
-`
 export default about
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,30 +1,11 @@
 import React from "react"
 import { Link } from "gatsby"
 import Layout from "../components/Layout"
-import { page, text } from "../examples/home.module.css"
-import { ExampleButton } from "../examples/button"
 
 export default function Home() {
   return (
     <Layout>
-      <div className={page}>
-        <h1>Home page!</h1>
-        <ExampleButton>CLICK ME</ExampleButton>
-        <h1>hello world</h1>
-        <h1>hello people</h1>
-        <p className={text}>
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quis modi
-          totam enim corporis harum sed, magni aspernatur distinctio optio
-          molestias iure repellendus laudantium vel quo dolorem quidem impedit
-          odit itaque!
-        </p>
-      </div>
-      <div>
-        <Link to="/about">about</Link>
-      </div>
-      <div>
-        <Link to="/company/history">history</Link>
-      </div>
+      <h1>Home page!</h1>
     </Layout>
   )
 }
