13a905be2e80675220c952f7d73a7d1a7465b2c7
adriana <adriana.mihalescu@gmail.com>
2021-12-17 16:39:21 +0000

    removed examples from the homepage and created the layout of the About page

--- src/examples/Images.js
+++ src/examples/Images.js
@@ -1,19 +1,65 @@
 import React from "react"
 import styled from "styled-components"
 import big from "../assets/images/big.jpg"
+import { StaticImage } from "gatsby-plugin-image"
+
+//trying out different component prop values , see more :@ https://www.gatsbyjs.com/docs/reference/built-in-components/gatsby-plugin-image/
 
 const Images = () => {
   return (
     <Wrapper>
-      <img src={big} alot="food" />
-      <h2>gatsby image</h2>
+      <article>
+        <h4>constrained / default</h4>
+        <StaticImage
+          src="../assets/images/recipe-1.jpeg"
+          alt="food"
+          placeholder="tracedSVG"
+          layout="constrained"
+          className="example-img"
+          as="section"
+        ></StaticImage>
+      </article>
+      <article>
+        <h4>fixed</h4>
+        <StaticImage
+          src="../assets/images/recipe-1.jpeg"
+          alt="food"
+          placeholder="blurred"
+          layout="fixed"
+          width={200}
+          className="example-img"
+          as="div"
+        ></StaticImage>
+      </article>
+      <article>
+        <h4>full width</h4>
+        <StaticImage
+          src="../assets/images/recipe-1.jpeg"
+          alt="food"
+          placeholder="dominantColor"
+          layout="fullWidth"
+          className="example-img"
+          as="article"
+        ></StaticImage>
+      </article>
     </Wrapper>
   )
 }
 
 const Wrapper = styled.section`
-  img {
-    width: 200px;
+  width: 70vw;
+  margin: 0 auto;
+  display: grid;
+  text-align: center;
+  gap: 2rem;
+  article {
+    border: 2px solid red;
+  }
+  .example-img {
+    border-radius: 1rem;
+  }
+  @media (min-width: 992px) {
+    grid-template-columns: 1fr 1fr 1fr;
   }
 `
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,11 @@
 import React from "react"
-import { Link } from "gatsby"
+
 import Layout from "../components/Layout"
-import Images from "../examples/Images"
 
 export default function Home() {
   return (
     <Layout>
       <h1>Home page!</h1>
-      <Images />
     </Layout>
   )
 }
