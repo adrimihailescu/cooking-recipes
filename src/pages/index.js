736a6ad5739526f8b152237c756a7b8e8e8724ca
adriana <adriana.mihalescu@gmail.com>
2021-12-20 16:49:21 +0000

    added new example on how to render image gallery

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,6 @@ import React from "react"
 
 import Layout from "../components/Layout"
 import { StaticImage } from "gatsby-plugin-image"
-import FetchData from "../examples/fetchData"
 
 export default function Home() {
   return (
@@ -23,7 +22,6 @@ export default function Home() {
             </div>
           </div>
         </header>
-        <FetchData />
       </main>
     </Layout>
   )
--- src/pages/testing.js
+++ src/pages/testing.js
@@ -1,36 +1,39 @@
 import React from "react"
-import { graphql } from "gatsby"
 
+import Layout from "../components/Layout"
+import Gallery from "../examples/Gallery"
 //using page query in pages NOT in the component
 
-const Testing = ({ data }) => {
-  const author = data.site.info.author
+const Testing = () => {
+  // const author = data.site.info.author
   return (
-    <div>
-      <h2>author: {author}</h2>
-    </div>
+    <Layout>
+      <main className="page">
+        <Gallery />
+      </main>
+    </Layout>
   )
 }
 
-export const data = graphql`
-  {
-    site {
-      info: siteMetadata {
-        author
-        description
-        simpleData
-        title
-        person {
-          age
-          name
-        }
-        complexData {
-          age
-          name
-        }
-      }
-    }
-  }
-`
+// export const data = graphql`
+//   {
+//     site {
+//       info: siteMetadata {
+//         author
+//         description
+//         simpleData
+//         title
+//         person {
+//           age
+//           name
+//         }
+//         complexData {
+//           age
+//           name
+//         }
+//       }
+//     }
+//   }
+// `
 
 export default Testing
