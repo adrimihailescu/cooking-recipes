f31fead5a4a2e6859b0c5826c26c821165bc31de
adriana <adriana.mihalescu@gmail.com>
2021-12-21 18:17:47 +0000

    added helper function and used it in the TagsList component

--- src/components/TagsList.js
+++ src/components/TagsList.js
@@ -1,9 +1,28 @@
 import React from "react"
+import setupTags from "../utils/setupTags"
+import { Link } from "gatsby"
 
-// this is the component that will render the tags
+// this is the component that will render the tags, we return a link to each tag/page
+//used a helper function setupTags
+//passing "recipes as props" to the component
 
-const TagsList = () => {
-  return <div>this is tags list</div>
+const TagsList = ({ recipes }) => {
+  const newTags = setupTags(recipes)
+  return (
+    <div className="tag-container">
+      <h4>recipes</h4>
+      <div className="tags-list">
+        {newTags.map((tag, index) => {
+          const [text, value] = tag
+          return (
+            <Link to={`/${text}`} key={index}>
+              {text}({value})
+            </Link>
+          )
+        })}
+      </div>
+    </div>
+  )
 }
 
 export default TagsList
