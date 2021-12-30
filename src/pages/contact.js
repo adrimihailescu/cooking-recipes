ce2b8cf3b86747422d8eb2be50e1e59e4dfb70ea
adriana <adriana.mihalescu@gmail.com>
2021-12-30 15:39:09 +0000

    added formspree to the contact page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,6 +1,6 @@
 import React from "react"
 import Layout from "../components/Layout"
-import { Link, graphql } from "gatsby"
+import { graphql } from "gatsby"
 import RecipesList from "../components/RecipesList"
 
 const Contact = ({ data }) => {
@@ -22,7 +22,11 @@ const Contact = ({ data }) => {
             </p>
           </article>
           <article>
-            <form action="" className="form contact-form">
+            <form
+              action="https://formspree.io/f/mzbozrqk"
+              method="POST"
+              className="form contact-form"
+            >
               <div className="form-row">
                 <label htmlFor="name">your name</label>
                 <input type="text" name="name" id="name" />
